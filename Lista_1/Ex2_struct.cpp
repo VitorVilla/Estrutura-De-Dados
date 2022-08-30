ff82cb3b3cb213a75c0a1c7da7f12f85ee8ef508
VitorVilla <vivilla1010@gmail.com>
2022-08-29 22:21:48 -0300

    Code update

--- Lista_1/Ex2_struct.cpp
+++ Lista_1/Ex2_struct.cpp
@@ -5,7 +5,54 @@
 
 using namespace std;
 
+struct Data
+{
+    int dia;
+    int mes;
+    int ano;
+};
+
+struct Pessoa
+{
+    string nome;
+    Data data;
+};
+
+Pessoa *cadastro(Pessoa user[2])
+{
+    for (int i = 0; i < 2; i++)
+    {
+        cout << "Digite o nome do usuário: ";
+        cin >> user[i].nome;
+
+        cout << "Digite o dia do seu nascimento: ";
+        cin >> user[i].data.dia;
+
+        cout << "Digite o mês do seu nascimento: ";
+        cin >> user[i].data.mes;
+
+        cout << "Digite o ano do seu nascimento: ";
+        cin >> user[i].data.ano;
+
+        cout << endl;
+    }
+    return user;
+}
+
 int main()
 {
+
+    Pessoa user[2];
+
+    user[2] = *cadastro(user);
+
+    for (int a = 0; a < 2; a++)
+    {
+        cout << "O nome é: " << user[a].nome << endl;
+        cout << "A data é: " << user[a].data.dia << "/" << user[a].data.mes << "/" << user[a].data.ano << endl;
+
+        cout << "-------------------------------------- \n";
+    }
+
     return 0;
 }
